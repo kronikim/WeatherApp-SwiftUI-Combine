bac06f38b9cc2506a67a2ae9f584bea37cd56803
deniz karahan <denizkarahan88@gmail.com>
2022-10-19 16:06:12 +0300

    Initial Commit

---
+++ WeatherApp-SwiftUI-Combine/WeatherApp_SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUI_CombineApp.swift
+//  WeatherApp-SwiftUI-Combine
+//
+//  Created by deniz karahan on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
